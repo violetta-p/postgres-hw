3506d3367ba1cd2180cec6ad9f21f9e763657d8b
ViolettaP <violetta.pukhova@mail.ru>
2023-07-12 21:58:08 +0300

    HW_1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employee
+(
+    employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+    title varchar(100) NOT NULL,
+	birth date,
+    notes text
+);
+
+CREATE TABLE customer
+(
+    id SERIAL PRIMARY KEY,
+    customer_id varchar(100) UNIQUE NOT NULL,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+	employee_id int NOT NULL REFERENCES employee(employee_id),
+    customer_id varchar(100) NOT NULL REFERENCES customer(customer_id),
+	order_date date,
+	ship_city varchar(100) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,111 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os
+import csv
+from abc import ABC, abstractmethod
+
+conn_password: str = os.getenv('POSTGRES_PW')
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password=conn_password
+)
+
+curs = conn.cursor()
+
+
+class Writer(ABC):
+    @abstractmethod
+    def write_data(self):
+        pass
+
+
+class OpenCSV:
+    def __init__(self, dir_name, file_name):
+        self.full_path_to_data = os.path.join(os.path.abspath("."), dir_name, file_name)
+
+    def get_data_from_csv(self):
+        all_data = []
+        with open(self.full_path_to_data, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                all_data.append(row)
+            print(all_data)
+
+            return all_data
+
+
+class Employees(OpenCSV, Writer):
+
+    def __init__(self, dir_name, file_name):
+        super().__init__(dir_name, file_name)
+
+    def write_data(self) -> None:
+        employees_data = self.get_data_from_csv()
+        for item in employees_data:
+            all_data = (
+                item["employee_id"], item["first_name"],
+                item["last_name"], item["title"],
+                item["birth_date"], item["notes"],
+            )
+
+            curs.execute("INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s)", all_data)
+            conn.commit()
+
+
+class Customers(OpenCSV, Writer):
+
+    def __init__(self, dir_name, file_name):
+        super().__init__(dir_name, file_name)
+
+    def write_data(self) -> None:
+        customers_data = self.get_data_from_csv()
+        for item in customers_data:
+            all_data = (
+                item["customer_id"],
+                item["company_name"],
+                item["contact_name"],
+            )
+
+            curs.execute(f"INSERT INTO customer VALUES (DEFAULT,%s, %s, %s)", all_data)
+            conn.commit()
+
+
+class Orders(OpenCSV, Writer):
+    EMPLOYEES_DATA_FILE = "employees_data.csv"
+
+    def __init__(self, dir_name, file_name):
+        super().__init__(dir_name, file_name)
+
+    def write_data(self):
+        orders_data = self.get_data_from_csv()
+        for item in orders_data:
+            all_data = (
+                item["order_id"], item["employee_id"],
+                item["customer_id"], item["order_date"],
+                item["ship_city"],
+            )
+
+            curs.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", all_data)
+            conn.commit()
+
+
+if __name__ == "__main__":
+
+    DIR_NAME = "north_data"
+    EMPLOYEES_DATA_FILE = "employees_data.csv"
+    CUSTOMERS_DATA_FILE = "customers_data.csv"
+    ORDERS_DATA_FILE = "orders_data.csv"
+
+    OpenCSV(DIR_NAME, EMPLOYEES_DATA_FILE)
+    emp = Employees(DIR_NAME, EMPLOYEES_DATA_FILE)
+    emp.write_data()
+
+    OpenCSV(DIR_NAME, EMPLOYEES_DATA_FILE)
+    cust = Customers(DIR_NAME, CUSTOMERS_DATA_FILE)
+    cust.write_data()
+
+    OpenCSV(DIR_NAME, ORDERS_DATA_FILE)
+    order = Orders(DIR_NAME, ORDERS_DATA_FILE)
+    order.write_data()
